e0954297eb39f218649b3f87d8774b009e15a6f0
Prasun Das <58937669+Prasundas99@users.noreply.github.com>
2021-03-25 16:40:44 +0530

    Formatted

--- Challenge3/index.php
+++ Challenge3/index.php
@@ -3,8 +3,4 @@
 <form class="box" action="flag.php" method="get">
   <h1>MAY THE SOURCE BE WITH YOU</h1>
   <h3>Credits to the original author of the challenge</h3>
-  <a href = "Challenge3\source.txt">Pseudo Code</a>
-
-  <input type="password" name="passwd" placeholder="Password">
-  <input type="submit" value="LOGIN" />
-</form>
+  <a href = "Challenge3\source.txt">Pseudo Code</a><input type="password" name="passwd" placeholder="Password"><input type="submit" value="LOGIN" /></form>
